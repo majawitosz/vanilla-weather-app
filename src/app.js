1cbfb54e28ef1a240a69ab7dfbf87135399d3acc
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-04-25 23:09:18 +0200

    Adding Twemoji

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+var div = document.createElement("div");
+div.textContent = "I \u2764\uFE0F emoji!";
+document.body.appendChild(div);
 
+twemoji.parse(document.body);
+
+var img = div.querySelector("img");
+
+// note the div is preserved
+img.parentNode === div; // true
+
+img.src; // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
+img.alt; // \u2764\uFE0F
+img.className; // emoji
+img.draggable; // false
+
+function imageSourceGenerator(icon, options) {
+  return "".concat(
+    options.base, // by default Twitter Inc. CDN
+    options.size, // by default "72x72" string
+    "/",
+    icon, // the found emoji as code point
+    options.ext // by default ".png"
+  );
+}
