e2d2af927b9c119b73ee26dbae195b0c56d79fcb
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-04-26 10:21:47 +0200

    Getting Axios and API

--- src/app.js
+++ src/app.js
@@ -1,25 +1,8 @@
-var div = document.createElement("div");
-div.textContent = "I \u2764\uFE0F emoji!";
-document.body.appendChild(div);
-
-twemoji.parse(document.body);
-
-var img = div.querySelector("img");
-
-// note the div is preserved
-img.parentNode === div; // true
-
-img.src; // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
-img.alt; // \u2764\uFE0F
-img.className; // emoji
-img.draggable; // false
-
-function imageSourceGenerator(icon, options) {
-  return "".concat(
-    options.base, // by default Twitter Inc. CDN
-    options.size, // by default "72x72" string
-    "/",
-    icon, // the found emoji as code point
-    options.ext // by default ".png"
-  );
+function displayTemperature(response) {
+  console.log(response.data.list[0].main.temp);
 }
+
+let apiKey = "4bf607af66f424ce009f3ab41fd57579";
+let apiUrl = `https://api.openweathermap.org/data/2.5/find?q=New%20York&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
