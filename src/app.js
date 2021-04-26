c75d857c865994689e41af10c7a1b0295b93e6cf
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-04-26 15:21:58 +0200

    Showing the current temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data.list[0].main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.list[0].main.temp);
+  cityElement.innerHTML = response.data.list[0].name;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.list[0].main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.list[0].wind.speed);
+  let pressureElement = document.querySelector("#air-pressure");
+  pressureElement.innerHTML = response.data.list[0].main.pressure;
 }
 
 let apiKey = "4bf607af66f424ce009f3ab41fd57579";
