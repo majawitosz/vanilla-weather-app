fa33ee84ca67cfef39fb23e00dd525be4da082df
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-04-27 19:21:58 +0200

    Adding weather icons imgs

--- src/app.js
+++ src/app.js
@@ -36,27 +36,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.list[0].wind.speed);
   pressureElement.innerHTML = response.data.list[0].main.pressure;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/${weatherIcon}.png`
-  );
-  let weatherIcon = response.data.list[0].weather.icon;
+  let weatherIcon = response.data.list[0].weather[0].icon;
+  iconElement.setAttribute("src", `src/images/${weatherIcon}.png`);
 }
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun_2600-fe0f.png   
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun-behind-cloud_26c5.png 
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/cloud-with-rain_1f327-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/cloud-with-lightning_1f329-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/snowflake_2744-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/fog_1f32b-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/cloud_2601-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun-behind-rain-cloud_1f326-fe0f.png
-// https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/cloud-with-lightning-and-rain_26c8-fe0f.png
-let icons = [
-
-]
 
 let apiKey = "4bf607af66f424ce009f3ab41fd57579";
-let city = "Paris";
+let city = "Katowice";
 let apiUrl = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
